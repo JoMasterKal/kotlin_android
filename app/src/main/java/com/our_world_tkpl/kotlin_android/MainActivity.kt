724444b15d407bb007bb49d9fe1c3d9b28a7381b
Jonathan kalonga <jokalmwamba@gmail.com>
2020-06-26 15:59:55 +0200

    get the xml layout with findViewById<>(), create function addNickname and add an execution to a button

--- app/src/main/java/com/our_world_tkpl/kotlin_android/MainActivity.kt
+++ app/src/main/java/com/our_world_tkpl/kotlin_android/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.our_world_tkpl.kotlin_android
 
+import android.content.Context
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_btn).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickName_edit)
+        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nickNameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nickNameTextView.visibility = View.VISIBLE
+
+        //hide a keyboad.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
