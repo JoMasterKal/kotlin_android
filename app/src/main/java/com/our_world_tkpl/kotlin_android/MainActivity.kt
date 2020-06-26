5269ace22ed3bb0be480f81bbfcadd69b823c3bc
Jonathan kalonga <jokalmwamba@gmail.com>
2020-06-26 16:36:21 +0200

    create a databinding variable and change a all varible that use findviewbyId to databinding

--- app/src/main/java/com/our_world_tkpl/kotlin_android/MainActivity.kt
+++ app/src/main/java/com/our_world_tkpl/kotlin_android/MainActivity.kt
@@ -9,25 +9,36 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.our_world_tkpl.kotlin_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_btn).setOnClickListener {
+//        findViewById<Button>(R.id.done_btn).setOnClickListener {
+//            addNickname(it)
+//        }
+        binding.doneBtn.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickName_edit)
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+//        val editText = findViewById<EditText>(R.id.nickName_edit)
+//        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nickNameTextView.visibility = View.VISIBLE
+        binding.apply {
+            nicknameText.text = binding.nickNameEdit.text
+            nickNameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         //hide a keyboad.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
