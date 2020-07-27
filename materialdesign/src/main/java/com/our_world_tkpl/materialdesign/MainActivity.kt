c4207fcac6a1bd856423807b71fbb02b331bda39
Jonathan kalonga <jokalmwamba@gmail.com>
2020-07-27 17:44:25 +0200

    confing off mainapp

--- materialdesign/src/main/java/com/our_world_tkpl/materialdesign/MainActivity.kt
+++ materialdesign/src/main/java/com/our_world_tkpl/materialdesign/MainActivity.kt
@@ -2,12 +2,33 @@ package com.our_world_tkpl.materialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.shr_main_activity.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationHost {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        setContentView(R.layout.shr_main_activity)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.contener,LoginFragment())
+                .commit()
+        }
+
+    }
+
+    override fun navigateTo(fragment: Fragment, addToBackStack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.contener, fragment)
+
+        if (addToBackStack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
     }
 }
