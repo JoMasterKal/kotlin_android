7e3fa8e9ec153c85b417f3e7dd60ee4f796cd877
Jonathan kalonga <jokalmwamba@gmail.com>
2020-07-29 23:55:21 +0200

    set menu

--- materialdesign/src/main/java/com/our_world_tkpl/materialdesign/ProductGridFragment.kt
+++ materialdesign/src/main/java/com/our_world_tkpl/materialdesign/ProductGridFragment.kt
@@ -1,18 +1,31 @@
 package com.our_world_tkpl.materialdesign
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_product_grid.view.*
 
 class ProductGridFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_grid, container, false)
+        val view = inflater.inflate(R.layout.fragment_product_grid, container, false)
+
+        //set up toolBar
+        (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.shr_toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
