f55252bfc939b7dfa24cf90c7af7d999b99d1007
Jonathan kalonga <jokalmwamba@gmail.com>
2020-08-03 16:15:04 +0200

    add variable to hold data

--- materialdesign/src/main/java/com/our_world_tkpl/ProductCardViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.our_world_tkpl
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class ProductCardViewHolder(itemView: View) //TODO: Find and store views from itemView
-    : RecyclerView.ViewHolder(itemView)
