aef430d1e95e9c83490f4baf53a81b42a3dfbfb1
Jonathan kalonga <jokalmwamba@gmail.com>
2020-08-03 16:16:01 +0200

    add decoration for recyclerview

--- materialdesign/src/main/java/com/our_world_tkpl/materialdesign/ProductGridItemDecoration.kt
+++ materialdesign/src/main/java/com/our_world_tkpl/materialdesign/ProductGridItemDecoration.kt
@@ -4,7 +4,8 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class ProductGridItemDecoration(private val largePadding: Int, private val smallPadding: Int) : RecyclerView.ItemDecoration() {
+class ProductGridItemDecoration(private val largePadding: Int,
+                                private val smallPadding: Int) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(outRect: Rect, view: View,
                                 parent: RecyclerView, state: RecyclerView.State) {
